498b04405d41f05333c0b433f37c6f8fd55b2299
Alisa Aylward <aaylward@fitbit.com>
2017-06-05 19:29:10 -0400

    logic for adding new users

--- database/create_new_user.js
+++ database/create_new_user.js
@@ -1,5 +1,6 @@
 // adapted from https://howtofirebase.com/save-and-query-firebase-data-ed73fb8c6e3a
 //check if exists reference: https://gist.github.com/anantn/4323949
+
 var firebase = require('firebase');
 firebase.initializeApp({
  "appName": "AIzaSyCl4pd35HfQuBTph08pgG2S9qXTFRZn1Ag",
@@ -11,19 +12,35 @@ firebase.initializeApp({
 var ref = firebase.app().database().ref();
 var usersRef = ref.child('users');
 
-var user_login = "admin";
+
+var user_login = "ALISA2";
 var user_first_name = "Z";
-var user_last_name =  "User";
+var user_last_name =  "XdsasX";
 var user_role = "admin";
 
-function create_new_user(user_login, user_first_name, user_last_name, user_role) {
-usersRef.set({
-  [user_login]: {
-    first_name: user_first_name,
-    last_name: user_last_name,
-    role: user_role,
-  }
-});
+
+var check_user = function(user_login,user_first_name, user_last_name, user_role){
+  usersRef.child(user_login).once('value', function(snapshot) {
+      var exists = (snapshot.val() !== null);
+      console.log(exists);
+      if (!exists) {
+        create_user(user_login,user_first_name, user_last_name, user_role)
+      }
+      else {
+        process.exit()
+      };
+    });
+};
+
+var create_user = function(user_login, user_first_name, user_last_name, user_role){
+      usersRef.update({
+        [user_login]: {
+        first_name: user_first_name,
+        last_name: user_last_name,
+        role: user_role,
+        }
+      });
+    return 1;
 }
 
-create_new_user(user_login, user_first_name, user_last_name, user_role)
+check_user(user_login,user_first_name, user_last_name, user_role);
