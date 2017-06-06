5ea07bf2a83f8b58a86dbc4c850be34c61af75de
Alisa Aylward <aaylward@fitbit.com>
2017-06-06 15:17:54 -0400

    adding write to users db

--- database/sign_in_with_credentials.js
+++ database/sign_in_with_credentials.js
@@ -11,19 +11,30 @@ var ref = firebase.app().database().ref();
 var usersRef = ref.child('users');
 
 
-var user_email = 'araylw12@gmail.com'
-var user_pass = 'abcdef5'
+var user_email = 'araylw12@gmail.com';
+var user_pass = 'abcdef5';
 
 
-var create_user = function(user_email, user_pass){
+function sign_in_user(user_email, user_pass){
 		firebase.auth().signInWithEmailAndPassword(user_email, user_pass).catch(function(error) {
-		  // Handle Errors here.
 		  var errorCode = error.code;
 		  var errorMessage = error.message;
 		  console.log(errorMessage);
-		  // ...
 		});
-      console.log("User " + user_email + " signed in at " + Date.now())
-}
 
-create_user(user_email, user_pass);
+		firebase.auth().onAuthStateChanged(function(user) {
+		  if (user) {
+		    console.log("user " + user.uid + " logged in");
+		          usersRef.update({
+			        [user.uid]: {
+			        display_name: user.displayName,
+			        last_login_dtm: Date.now(),
+			        }
+     		});
+      		return;
+		  } 
+		});
+		return;
+};
+
+sign_in_user(user_email, user_pass);
